e198202242350ecc57ff80834f080bce3474a7f7
Sameer-VTU <vtu25541@veltech.edu.in>
2026-05-09 23:44:55 +0530

    Time: 0 ms (100%), Space: 44.1 MB (73.87%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Create a dummy node to act as the starting point
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        // While both lists have nodes remaining
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        // If one list is exhausted, attach the remainder of the other list
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        // Return the actual head (ignoring the dummy node)
+        return dummy.next;
+    }
+}
